db0fc37fca488465f38d0b5862b84b658d15d411
John Shepard <john.shepard@blackfynn.com>
2020-12-08 09:09:04 -0500

    #grwfyg Add AdminGetUser and AdminCreateUser.

--- README.md
+++ README.md
@@ -4,14 +4,20 @@ Do not use this in production ever.
 
 It's only usage should be to test JWT/JWKS integrations for your backend apis.
 
-### usage
+## usage
 
-#### `GET`
+### `GET`
 
 Make a `GET` request to the server to receive a valid JWKS
 
-#### `POST`
+### `POST`
 
+#### AdminGetUser
+
+ * Include `NotFound` in the email address to get a 400 response.
+ * Include `confirmed` in the email address to get user status of CONFIRMED
+
+#### JWT
 Make a `POST` request to the server to receive a valid JWT signed with the private side of the public key stored in the JWKS
 
 Send a JSON body to add claims to the token
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { createServer } = require("http");
 const { readFileSync } = require("fs");
 const { sign } = require("jsonwebtoken");
 const concat = require("concat-stream");
+const uuid = require("uuid-random");
 
 const key = readFileSync("/etc/jwks-jwt/keys/private.key");
 const jwks = require("/etc/jwks-jwt/data/jwks.json");
@@ -14,16 +15,126 @@ const ALG = "RS256";
 const sendJWT = createJWTController(key);
 const sendJWKS = createJWKSController(jwks);
 
+const emptyData = "{}";
+const emptyJson = JSON.parse(emptyData)
+const apiUrl = "/dummy-user-pool/"
+
+const adminGetUserTarget = "AWSCognitoIdentityProviderService.AdminGetUser"
+const adminCreateUserTarget = "AWSCognitoIdentityProviderService.AdminCreateUser"
+
 const server = createServer((req, res) => {
-  switch (req.method.toLowerCase()) {
-    case "get":
-      return sendJWKS(req, res);
-    case "post":
-      return sendJWT(req, res);
-    default:
-      return res.end();
+  const requestMethod = req.method.toLowerCase();
+  const requestUrl = req.url.toLowerCase();
+  const target = req.headers["x-amz-target"]
+
+  if (requestMethod === "get") {
+    // Return the public keys
+    // URL will be / or /dummy-user-pool/.well-known/jwks.json
+    return sendJWKS(req, res);
   }
+
+  if (requestMethod !== "post") {
+    return res.end();
+  }
+
+  // Not an API call.
+  if (requestUrl !== apiUrl) {
+    return sendJWT(req, res);
+  }
+
+  let requestData = '';
+  req.on('data', chunk => {
+    requestData += chunk;
+  })
+  req.on('end', () => {
+    return apiCall(target, requestData, requestUrl, res)
+  })
 }).listen(8088);
+
+function apiCall(target, requestData, url, res) {
+  let jsonData;
+  try {
+    jsonData = JSON.parse(requestData);
+  } catch (exception) {
+    jsonData = emptyJson;
+  }
+
+  if (target === adminGetUserTarget) {
+    return adminGetUser(jsonData["Username"], res)
+  }
+
+  if (target === adminCreateUserTarget) {
+    return adminCreateUser(jsonData["Username"], res)
+  }
+
+  console.log("Unsupported API Call:", target, jsonData)
+
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Content-Length": emptyData.length
+  });
+
+  return res.end(emptyData);
+}
+
+function adminGetUser(email, res) {
+  if (email.toLowerCase().indexOf("notfound") !== -1) {
+
+    res.writeHead(400, {
+      "x-amzn-ErrorType": "UserNotFoundException"
+    });
+
+    return res.end();
+  }
+
+  const currentTime = Date.now();
+  const status = email.toLowerCase().indexOf("confirmed") === -1 ? "UNCONFIRMED" : "CONFIRMED";
+
+  const data = `{
+    "Enabled": true,
+    "MFAOptions": [],
+    "PreferredMfaSetting": "None",
+    "UserAttributes": [],
+    "UserCreateDate": ${currentTime},
+    "UserLastModifiedDate": ${currentTime},
+    "UserMFASettingList": [],
+    "Username": "${email}",
+    "UserStatus": "${status}"
+  }`;
+
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Content-Length": data.length
+  });
+
+  return res.end(data);
+}
+
+function adminCreateUser(email, res) {
+  const currentTime = Date.now();
+  const status = "FORCE_CHANGE_PASSWORD";
+
+  const data = `{
+    "User": {
+      "Attributes": [
+        {"Name": "sub", "Value": "${uuid()}"}
+      ],
+      "Enabled": true,
+      "MFAOptions": [],
+      "UserCreateDate": ${currentTime},
+      "UserLastModifiedDate": ${currentTime},
+      "Username": "${email}",
+      "UserStatus": "${status}"
+    }
+  }`;
+
+  res.writeHead(201, {
+    "Content-Type": "application/json",
+    "Content-Length": data.length
+  });
+
+  return res.end(data);
+}
 
 function createJWTController(key) {
   return function sendJWT(req, res) {
